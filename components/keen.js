cd7ac6eb374c9b995da5803ecbc0145590620882
Kyle Shockey <kyle.shockey1@gmail.com>
2016-06-09 03:49:54 -0700

    init

--- components/keen.js
+++ components/keen.js
@@ -1,10 +1,7 @@
-const Keen = require('keen.io')
+const config = require('../config')
+const request = require('request')
 
 
-const keen = Keen.configure({
-	projectId: "5757f5ae80a7bd042843f2fd",
-	writeKey:"c37ec921633f983e2bc120f1ce8141e848c3954e81a44721eabfd9347af4537e420a549b00aa19b2e6e487dc72ecfbdb841acde1d1c78801e210ac2c18c30757bfe6241961dae93d813ca8c3e545161f1ca92f30bc4b3502cba5a857d8468453"
-})
 
 // send anonymous usage stats to the tank team
 function track(evt, data, fn) {
@@ -12,10 +9,17 @@ function track(evt, data, fn) {
     return
   }
   fn = fn || function() {}
-  keen.addEvent(evt, data, function(err) {
-    if(!err) {
-      fn()
+  console.log(config.server + 'track')
+  request.put(config.server + 'track', {
+    json: true,
+    body: {
+      evt: evt,
+      data: data
     }
+  }, function(err) {
+      if(!err) {
+        fn()
+      }
   })
 }
 
